bc77f2b8f73c6c699de553f5b411003c41f47a2f
Baicheng Yin <y@yinbaicheng.com>
2024-11-16 17:37:22 +0000

    api template added

--- flask_backend/main.py
+++ flask_backend/main.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
   return "Hello World!"
+
+@app.route("/echo", methods=["POST"])
+def echo():
+  return request.json
+
+@app.route("/deliberation", methods=["POST"])
+def deliberation():
+    return {
+       "metadata": "this is an example deliberation",
+        "conversation": [
+            {
+                "agent_name": "Alex",
+                "transcript": "I think I'm actually the criminal."
+            },
+            {
+                "agent_name": "Bob",
+                "transcript": "Wait what?"
+            },
+            {
+                "agent_name": "Charlie",
+                "transcript": "I think I'm actually the criminal."
+            }
+        ],
+        "vote": [
+            {
+                "agent_name": "Alex",
+                "verdict": "guilty"
+            },
+            {
+                "agent_name": "Bob",
+                "verdict": "not_guilty"
+            },
+            {
+                "agent_name": "Charlie",
+                "verdict": "guilty"
+            }
+        ]
+    }
 
 if __name__ == "__main__":
   app.run(debug=True)
